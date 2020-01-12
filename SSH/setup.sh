9feff669bf5ce7a3cf3b8ce2b316a9a059aa904d
LinhNV <vietlinhtspt@gmail.com>
2020-01-12 07:55:19 +0700

    Add scrip download model

--- SSH/setup.sh
+++ SSH/setup.sh
@@ -3,8 +3,16 @@ cd tf-faster-rcnn/lib
 make clean
 make
 cd ..
+
 cd data
 git clone https://github.com/pdollar/coco.git
 cd coco/PythonAPI
 make
 cd ../../..
+
+mkdir -p data/imagenet_weights
+cd data/imagenet_weights
+wget -v http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
+tar -xzvf vgg_16_2016_08_28.tar.gz
+mv vgg_16.ckpt vgg16.ckpt
+cd ../..
